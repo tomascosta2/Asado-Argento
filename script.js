1366f8b88d530558725476160f9d2c6e0ecc0282
Tomas Costa <tomascostapp@gmail.com>
2022-05-17 17:30:17 -0300

    Tercer Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('nav a').
+
+window.onload = function() {
+    document.getElementById("page").style.backgroundColor='orangered';
+}
+
+// forEach(link => {
+
+//     console.log(link.href)
+//     // if(link.href.includes(`${activePage}`)){
+//     //     console.log(`${activePage}`);
+//     // }
+// })
